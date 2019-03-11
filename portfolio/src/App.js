d3bfd06a380258c9131e1fcf96a2b73f83558351
Davidson Silva <davidson.f.silva@gmaill.com>
2019-03-11 15:06:34 -0300

    Code: Social Profiles Component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Projects from './Projects';
+import SocialProfiles from  './SocialProfiles';
 
 import './App.css';
 
@@ -33,6 +34,8 @@ class App extends Component {
         }
         <hr/>
         <Projects/>
+        <hr />
+        <SocialProfiles />
       </div>
     );
   }
